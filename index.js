a57b8c55a48c3961bd025ccc8ae86a0930a88662
[jangmijin] <tlrud0621@gmail.com>
2020-09-03 18:03:35 +0900

    express 서버추가 및 라우터 설정 localhost에서 테스트

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hi');
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req,res) {
+    console.log(req);
+    res.send('hello from home');
+}
+
+function handleProfile(req,res) {
+    res.send('you are on my profile');
+}
+
+app.get("/",handleHome);
+
+app.get("/profile",handleProfile);
+
+app.listen(PORT, handleListening);
